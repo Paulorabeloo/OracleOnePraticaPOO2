bdf324858c7c69ea60305654b86bc8c09e1640bd
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2025-07-01 08:38:41 -0300

    exercicio2

--- src/dois/atividade/IdadePessoa.java
+++ src/dois/atividade/IdadePessoa.java
@@ -1,4 +1,31 @@
 package dois.atividade;
 
 public class IdadePessoa {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void verificarIdade(int idade){
+        if(idade >= 18){
+            System.out.println("É maior de idade");
+        } else {
+            System.out.println("É menor de idade");
+        }
+    }
+
 }
---
+++ src/dois/atividade/Main.java
@@ -0,0 +1,11 @@
+package dois.atividade;
+
+public class Main {
+    public static void main(String[] args) {
+        IdadePessoa p1 = new IdadePessoa();
+        IdadePessoa p2 = new IdadePessoa();
+
+        p1.verificarIdade(20);
+        p2.verificarIdade(16);
+    }
+}
