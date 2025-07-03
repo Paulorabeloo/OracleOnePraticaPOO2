874baf6bebd273a784ceec2284072d8b3bfd5940
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2025-07-03 09:38:21 -0300

    exercicio4

--- src/tres/atividade/Main.java
+++ src/tres/atividade/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Produto p1 = new Produto();
 
-        p1.setPreco(2500);
+        p1.setPreco(5000);
         System.out.println(p1.aplicarDesconto(10));
-
     }
 }
